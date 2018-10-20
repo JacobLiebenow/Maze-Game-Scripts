82731e2b80100bb2bad4468c5e8d11f6719566b8
Jacob Liebenow <liebenow.jacob@gmail.com>
2018-10-20 19:02:54 -0400

    Initial commit

---
+++ PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text winText;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            other.gameObject.SetActive(false);
+            winText.text = "You win!";
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+}
